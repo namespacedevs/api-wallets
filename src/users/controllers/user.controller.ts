45f0dbd97c574ee29d6cac981e60d0a58fb22ba5
e <thiagobarros95@gmail.com>
2021-10-05 19:40:38 -0300

    Adiciona os métodos @POST/create, @PUT/update, cria o DTO create-user.dto.ts, melhora o método @DELETE/delete finalizando o CRUD básica do module users

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -2,10 +2,12 @@ import {
     Controller,
     Delete,
     Get,
-    //Put, 
-    //Post,
+    Put,
+    Post,
     Param,
-    } from "@nestjs/common";
+    Body,
+} from "@nestjs/common";
+import { CreateUserDto } from "../dtos/create-user.dto";
 import { UserModel } from "../model/user.model";
 import { UserService } from "../providers/user.service";
 
@@ -14,29 +16,29 @@ export class UserController {
 
     constructor(
         private userService: UserService
-        ){ }
+    ) { }
 
     @Get()
-    async findAll() : Promise<UserModel[]>{
+    async findAll(): Promise<UserModel[]> {
         return this.userService.findAll();
     }
     @Get(':document')
-    async findOne(@Param('document') document: number) : Promise<UserModel>{
+    async findOne(@Param('document') document: number): Promise<UserModel> {
         return this.userService.findOne(document);
     }
-    //@Post()
-    //create() {
-       //return;
-    //}
+    @Post()
+    create(@Body() createDto: CreateUserDto) {
+        return this.userService.create(createDto);
+    }
 
-    //  @Put(':document')
-    //update(@Param('document') document: number) {
-        //return this.userService.update(+document);
-    //}
+    @Put(':document')
+    update(@Param('document') document: number, @Body() updateUserDto: CreateUserDto) {
+        console.log(updateUserDto);
+        return this.userService.update(document, updateUserDto);
+    }
 
     @Delete(':document')
-    async  delete(@Param('document') document: number) {
-        this.userService.delete(document);
-        console.log("User " + document + " removido")
+    async delete(@Param('document') document: number) {
+        return this.userService.delete(document); 
     }
 }
--- src/users/providers/user.service.ts
+++ src/users/providers/user.service.ts
@@ -1,32 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../dtos/create-user.dto';
 import { UserModel } from '../model/user.model';
 
 @Injectable()
-export class UserService{
+export class UserService {
     users: UserModel[] = [
-        {name: 'Thiago', email: 'thiago@tesla.com', document: 12345678911 },
-        {name: 'Uigor', email: 'uigor@marshall.com', document: 12345678912},
-        {name: 'Jeff', email: 'jeff@tequila.com', document: 12345678913},
-        {name: 'Adisson', email: 'adisson@solteiro.com', document: 12345678914}
+        { name: 'Thiago', email: 'thiago@tesla.com', document: 12345678911 },
+        { name: 'Uigor', email: 'uigor@marshall.com', document: 12345678912 },
+        { name: 'Jeff', email: 'jeff@tequila.com', document: 12345678913 },
+        { name: 'Adisson', email: 'adisson@solteiro.com', document: 12345678914 }
     ];
 
-        findAll(){
-            return this.users;
+    findAll() {
+        return this.users;
     }
-        findOne(document: number){
-            const user = this.users.find((value) => value.document == document);
-            return user
+    findOne(document: number) {
+        const user = this.users.find((user) => user.document == document);
+        return user
     }
-        //create(user: UserModel){
-            
-    //}
-        //update(user: UserModel){
-
-    //}
-        delete(document: number){
-            const index = this.users.findIndex((value) => value.document == document)
-            this.users.splice(index, document);
+    create(createUserDto: CreateUserDto) {
+        this.users.push(createUserDto);
+        return this.users;
+    }
+    update(document: number, updateUserDto: CreateUserDto) {
+        const user = this.findOne(updateUserDto.document);
+        console.log(user)
+        user.name = updateUserDto.name;
+        user.email = updateUserDto.email;
+        user.document = updateUserDto.document;
+        this.delete(document);
+        this.create(user);
+        return this.users;
+    }
+    delete(document: number) {
+        const user = this.findOne(document);
+        if (user) {
+            this.remove(document);
+            return user;
+        }
+        return 'Usuario não encontrado, por favor passar um documento valido!'
     }
 
-
+    private remove(document) : void {
+        this.users = this.users.filter((user) => user.document != document);
+    }
 }
