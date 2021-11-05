dfa685b775b21dbdb8994fd2812eb5a59629cfc7
e <thiagobarros95@gmail.com>
2021-11-05 11:26:58 -0300

    feat: adiciona método createDto

--- src/users/providers/user.service.ts
+++ src/users/providers/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 import { User } from '../users.entity';
 
 @Injectable()
@@ -18,6 +19,10 @@ export class UserService {
     findOne(id: number): Promise<User> {
         return this.userRepository.findOne(id);
     }
+    createDto(createUserDto: CreateUserDto){
+        return this.userRepository.insert(createUserDto);
+    }
+
     async delete(id: number): Promise<void> {
         await this.userRepository.delete(id);
     }
