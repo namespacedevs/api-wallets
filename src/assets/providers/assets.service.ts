641b78e194ad1c8dfabf481d4648d553526ff626
thiagotesla <thiagobarros95@gmail.com>
2021-11-09 20:23:46 -0300

    feat: Cria o Assets Service

---
+++ src/assets/providers/assets.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Param } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Asset } from "../assets.entity";
+import { CreateAssetDto } from "../dtos/create-asset.dto";
+import { UpdateAssetDto } from "../dtos/update-asset.dto";
+
+
+@Injectable()
+export class AssetsService {
+    constructor(
+        @InjectRepository(Asset)
+        private assetsRepository: Repository<Asset>
+    ) { }
+
+    findAll() {
+        console.log(this.assetsRepository.find());
+        return this.assetsRepository.find();
+
+    }
+    findOne(id: number): Promise<Asset> {
+        return this.assetsRepository.findOne(id);
+    }
+    create(create: CreateAssetDto){
+        return this.assetsRepository.create(create);
+    }
+    update(@Param('id') id: number,  update: UpdateAssetDto) {
+        return this.assetsRepository.update(id, update);
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.assetsRepository.delete(id);
+    }
+}
