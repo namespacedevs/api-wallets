a3d3c74040975f46f225f71c9716afcf3681bf34
thiagotesla <thiagobarros95@gmail.com>
2021-11-09 20:24:46 -0300

    fix: refatora User Module para remover código desnecessário

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './providers/users.service';
-import { User as User } from './users.entity';
+import { User } from './users.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
