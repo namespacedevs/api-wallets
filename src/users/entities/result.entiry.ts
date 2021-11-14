c91c7cfea5fa31f8d5ea4f7c88710cba36ca6b67
thiagotesla <thiagobarros95@gmail.com>
2021-11-14 20:30:29 -0300

    feat: cria result.entiry

---
+++ src/users/entities/result.entiry.ts
@@ -0,0 +1,11 @@
+export class Result {
+    constructor(
+        public message: string,
+        public success: boolean,
+        public data: any,
+        public errors: any,
+    ) {
+    
+    }
+
+}
