f1a88980856615d527af33a529b8fba7141fd493
thiagotesla <thiagobarros95@gmail.com>
2021-11-14 20:39:41 -0300

    fix: refatora users.contract

--- src/users/contracts/users.contract.ts
+++ src/users/contracts/users.contract.ts
@@ -6,13 +6,13 @@ import { Contract } from "./contract";
 
 @Injectable()
 export class UserContract implements Contract {
-    erros: any[];
+    errors: any[];
     validate(entity: User): boolean {
         const flunt = new Flunt();
         flunt.hasMinLen(entity.name, 3, 'O nome tem que ter pelo menos 3 letras.');
         flunt.isEmail(entity.email, 'Email inválido.');
         flunt.isFixedLen (entity.document, 11, 'O CPF tem que ter 11 números.');
-        this.erros = flunt.errors;
+        this.errors = flunt.errors;
         return flunt.isValid();
     }
 
