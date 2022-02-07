f97c885ec30aff46ef6ba0c7284fba9b4b3e87e0
thiagotesla <thiagobarros95@gmail.com>
2022-02-07 10:17:20 -0300

    refactor: padroniza resposta da requisição no users controller

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -12,26 +12,40 @@ import { UpdateUserContract } from "../contracts/update-user.contract";
 export class UsersController {
 
     constructor(
-        private userService: UsersService
+        private usersService: UsersService
     ) { }
 
     @Get()
-    findAll(): Promise<User[]> {
-        return this.userService.findAll();
-    }
-    @Get(':id')
-    findOne(@Param('id') id: number): Promise<User> {
-        return this.userService.findOne(id);
-    }
-    @Post()
-    @UseInterceptors(new ValidatorInterceptor(new CreateUserContract()))
-    async create(@Body() model: CreateUsersDto) {
+    async findAll() { 
         try{
-            const newUser = await this.userService.create(model);
-            return new Result('Usuário criado.', true, [newUser.name, newUser.email], null);
+            const users = await this.usersService.findAll();
+            return new Result('Lista dos usuários.', true, users, null);
         }
         catch(error){
-            throw new HttpException(new Result('Email ou CPF já cadastrados.', false, null, error), HttpStatus.BAD_REQUEST); 
+            throw new HttpException(new Result( 'Não foi possivel listar os usuários.', false, null, error), HttpStatus.BAD_REQUEST);
+        }    
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number){
+        try{
+            const user = await this.usersService.findOne(id);
+            return new Result('Usuário solicitado.', true, user, null);
+        }
+        catch(error){
+            throw new HttpException(new Result('Não foi possivel listar o usuário.', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Post()
+    @UseInterceptors(new ValidatorInterceptor(new CreateUserContract()))
+    async create(@Body() createUser: CreateUsersDto) {
+        try{
+            const newUser = await this.usersService.create(createUser);
+            return new Result('Usuário criado com sucesso.', true, newUser, null);
+        }
+        catch(error){
+            throw new HttpException(new Result('Não foi possivel criar o usuário.', false, null, error), HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -39,16 +53,22 @@ export class UsersController {
     @UseInterceptors(new ValidatorInterceptor(new UpdateUserContract()))
     async update(@Param('id') id: number, @Body() update: UpdateUserDto) {
         try{
-            const updateUser = await this.userService.update(id, update);
-            return new Result('Usuário criado.', true, updateUser, null);
+            const updateUser = await this.usersService.update(id, update);
+            return new Result('Usuário alterado com sucesso.', true, updateUser, null);
         }
         catch(error){
-            throw new HttpException(new Result('Email ou CPF já cadastrados.', false, null, error), HttpStatus.BAD_REQUEST); 
+            throw new HttpException(new Result('Não foi possivel alterar o usuário.', false, null, error), HttpStatus.BAD_REQUEST); 
         }
     }
 
     @Delete(':id')
-    async delete(@Param('id') id: number): Promise<void> {
-        return this.userService.delete(id);
+    async delete(@Param('id') id: number) {
+        try{
+            await this.usersService.delete(id);
+            return new Result('Usuário deletado com sucesso.', true, null, null);
+        }
+        catch(error){
+            throw new HttpException(new Result('Não foi possivel deletar o usuário.', false, null, error), HttpStatus.BAD_REQUEST); 
+        }
     }
 }
