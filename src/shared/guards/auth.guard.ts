de327205e63234716ad1051791bffc5ef2841b8b
thiagotesla <thiagobarros95@gmail.com>
2022-02-07 16:00:09 -0300

    refactor: remove codigos desnecessários auth guard

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -3,16 +3,6 @@ import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(){
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        return super.canActivate(context);
-    }
-
-    handleRequest<TUser = any>(err: any, user: any): TUser {
-        if(err || !user){
-            throw new UnauthorizedException();
-        }
-        return user;
-    }
-
+export class JwtAuthGuard extends AuthGuard('jwt'){
+    
 }
