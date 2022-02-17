97c96c8789d0da7c7d3584f9b670e1dde02b9e6d
thiagotesla <thiagobarros95@gmail.com>
2022-02-17 09:31:06 -0300

    feat: add parametros ao create-bank-account.dto

--- src/modules/bank-accounts/dtos/create-bank-account.dto.ts
+++ src/modules/bank-accounts/dtos/create-bank-account.dto.ts
@@ -1,1 +1,8 @@
-export class CreateBankAccountDto {}
+import { User } from "src/modules/users/entities/users.entity";
+
+export class CreateBankAccountDto {
+    constructor(
+        public bankName: string,
+        public userId: User,
+    ){}
+}
