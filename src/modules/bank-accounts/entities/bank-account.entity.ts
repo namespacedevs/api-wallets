08ee75bd3a7dba53cd2c57494bb4fdaeb136e861
thiagotesla <thiagobarros95@gmail.com>
2022-02-17 09:32:48 -0300

    feat: configura bank-account.entity

--- src/modules/bank-accounts/entities/bank-account.entity.ts
+++ src/modules/bank-accounts/entities/bank-account.entity.ts
@@ -1,1 +1,16 @@
-export class BankAccount {}
+import { User } from 'src/modules/users/entities/users.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
+
+@Entity('bank_account')
+export class BankAccount {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    bankName: string;
+
+    @ManyToOne(() => User, user => user.bankAccount)
+    @JoinColumn({ name: 'userId' })
+    userId: User;
+    bankAccount: any;
+}
