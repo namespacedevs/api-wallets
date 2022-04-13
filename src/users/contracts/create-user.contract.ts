ab1e5031a1a15bf6d463549f29e17bffab181875
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-13 10:50:15 -0300

    fix password contract error message

--- src/users/contracts/create-user.contract.ts
+++ src/users/contracts/create-user.contract.ts
@@ -11,7 +11,7 @@ export class CreateUserContract implements Contract {
         flunt.hasMinLen(entity.name, 3, 'O nome tem que ter pelo menos 3 letras.');
         flunt.isEmail(entity.email, 'Email inválido.');
         flunt.isFixedLen (entity.document, 11, 'O CPF tem que ter 11 números.');
-        flunt.hasMinLen(entity.password, 8, 'O nome tem que ter pelo menos 3 letras.');
+        flunt.hasMinLen(entity.password, 8, 'O nome tem que ter pelo menos 8 caracteres.');
         
         this.errors = flunt.errors;
 
