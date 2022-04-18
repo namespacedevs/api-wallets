bf94638c2aaf8d2c49e5e1574e8816c42ec74b10
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-04-18 10:07:25 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = require('cors')
+  app.use(cors())
   await app.listen(3000);
 }
 bootstrap();
