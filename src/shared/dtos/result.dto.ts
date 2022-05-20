960ce7068e398000262ba9085b5ac4edb3cf0b96
Thiago Barros <88802518+thiagotesla@users.noreply.github.com>
2022-05-20 00:42:34 -0300

    refactor/ altera nome do paramentro

--- src/shared/dtos/result.dto.ts
+++ src/shared/dtos/result.dto.ts
@@ -2,7 +2,7 @@ export class Result {
     constructor(
         public message: string,
         public success: boolean,
-        public resultData: any,
+        public data: any,
         public errors: any,
     ) { 
     }
